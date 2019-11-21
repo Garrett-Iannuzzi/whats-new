05da11d7152c6efdee476abe55ea106bac8c9eef
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-11-20 21:04:52 -0700

    Add snapshot test for render of APP and NewsArticle

---
+++ src/components/NewsArticle/NewsArticle.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NewsArticle from './NewsArticle';
+
+
+describe('News Article', () => {
+
+  it('should match the snapshot with all data passed in correctly', () => {
+    const wrapper = shallow(<NewsArticle
+      title='News'
+      img='//www.images.com'
+      description='No news is good news'
+      />);
+      expect(wrapper).toMatchSnapshot();
+  });
+
+});
