21091027bde1db80085ae3e09fbd70f38ef0aac6
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-11-21 16:14:49 -0700

    Add tests for all search buttons

--- src/components/Menu/Menu.test.js
+++ src/components/Menu/Menu.test.js
@@ -22,24 +22,32 @@ describe('Menu', () => {
     expect(wrapper).toMatchSnapshot()
   })
 
-  it('should filter by correct news type when filterByNewsType is clicked, first btn', () => {
+  describe('Search Buttons', () => {
+    it('should filter by correct news type when filterByNewsType is clicked, first btn', () => {
+      wrapper.find('button').at(0).simulate('click');
+      expect(mockFilterByNewsType).toHaveBeenCalled();
+    });
+  
+    it('should filter by correct news type when filterByNewsType is clicked, second btn', () => {
+      wrapper.find('button').at(1).simulate('click');
+      expect(mockFilterByNewsType).toHaveBeenCalled();
+    });
+  
+    it('should filter by correct news type when filterByNewsType is clicked, third btn', () => {
+      wrapper.find('button').at(2).simulate('click');
+      expect(mockFilterByNewsType).toHaveBeenCalled();
+    });
+  
+    it('should filter by correct news type when filterByNewsType is clicked, fourth btn', () => {
+      wrapper.find('button').at(3).simulate('click');
+      expect(mockFilterByNewsType).toHaveBeenCalled();
+    });
+  
+    it('should filter by correct news type when filterByNewsType is clicked, fifth btn', () => {
+      wrapper.find('button').at(4).simulate('click');
+      expect(mockFilterByNewsType).toHaveBeenCalled();
+    });
+  })
 
-  });
-
-  it('should filter by correct news type when filterByNewsType is clicked, second btn', () => {
-
-  });
-
-  it('should filter by correct news type when filterByNewsType is clicked, third btn', () => {
-
-  });
-
-  it('should filter by correct news type when filterByNewsType is clicked, fourth btn', () => {
-
-  });
-
-  it('should filter by correct news type when filterByNewsType is clicked, fifth btn', () => {
-
-  });
 
 })
