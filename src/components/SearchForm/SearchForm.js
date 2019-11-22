15237a638c2756dc78729fb10ef4aac76a54fb5a
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-11-21 23:32:49 -0700

    Add handleEnterKey function in search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -13,6 +13,12 @@ class SearchForm extends Component {
     this.setState({ inputValue: e.target.value })
   }
 
+  handleEnterKey = (e) => {
+    if (e.keyCode === 13) {
+      this.props.handleSearch(this.state.inputValue)
+    }
+  }
+
   render() {
     return (
       <div className='search-bar-div'>
@@ -20,9 +26,10 @@ class SearchForm extends Component {
           aria-label='Text Box'
           type='text'
           placeholder='Search News'
-          name='input'
+          name='inp'
           value={this.state.inputValue}
           onChange={this.handleChange}
+          onKeyDown={this.handleEnterKey} 
         />
         <button className='search-btn' onClick={() => this.props.handleSearch(this.state.inputValue)}>Search</button>
     </div>
