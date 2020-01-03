606a8b961783b85d77f89c38e80a03e840ecbe6e
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-01-03 12:42:35 -0700

    Add reducer/action for getArticles

---
+++ src/reducers/articles.js
@@ -0,0 +1,8 @@
+export const getArticles = (state = [], action) => {
+  switch(action.type) {
+    case 'GET_ARTICLES':
+      return [...state, ...action.articles]
+    default: 
+      return state
+  }
+}
